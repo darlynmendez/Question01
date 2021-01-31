bd8d97f581118e9a7e6f1c9743342f1a1c0188df
darlynmendez <77795140+darlynmendez@users.noreply.github.com>
2021-01-31 15:24:36 -0500

    updates

--- src/question01/Question01.java
+++ src/question01/Question01.java
@@ -17,6 +17,10 @@ public class Question01 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        int a = 5;
+        int b = 6;
+        int sum = a + b;
+        System.out.println(sum);
     }
     
 }
